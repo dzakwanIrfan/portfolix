09b8cad061dfe09eeceed7d8067987cd0d59651b
Dzakwan Irfan <lexdani368@gmail.com>
2025-02-27 22:13:51 +0700

    Add Hero component to enhance the main application layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from './components/Header'
+import { Hero } from './components/Hero'
 import './index.css'
 
 function App() {
   return (
     <div className="font-raleway">
       <Header />
+      <Hero />
     </div>
   )
 }
